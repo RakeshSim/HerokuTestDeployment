dd6c8c4917dd5b7e7a4c181da832c66cba5087b2
Rakesh Yadav <rakesh.qwert@gmail.com>
2020-01-22 19:16:50 -0700

    first commit

---
+++ src/test/java/com/example/easynotes/HelloWorldApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.easynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	
+
+}
